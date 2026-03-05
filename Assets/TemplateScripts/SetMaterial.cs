652d25da77fb3233eabb77fdc1997cc5fdec52b8
AndrewASO <andrew.skevington-24@ncf.edu>
2026-03-05 16:41:26 -0500

    Fixed Library scene. Didn't implement the SetMaterial changes from Library branch

---
+++ Assets/TemplateScripts/SetMaterial.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMaterial : MonoBehaviour
+{
+    public Material[] materials;
+
+    private int currentIndex = 0;
+    private MeshRenderer meshRenderer;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public void UpdateMaterial(Material newMat)
+    {
+        GetComponent<MeshRenderer>().material = newMat;
+    }
+
+    public void NextMaterial()
+    {
+        if (materials.Length == 0) return;
+
+        currentIndex++;
+
+        if (currentIndex >= materials.Length)
+        {
+            currentIndex = 0;
+        }
+
+        meshRenderer.material = materials[currentIndex];
+    }
+
+    public void PreviousMaterial()
+    {
+        if (materials.Length == 0) return;
+
+        currentIndex--;
+
+        if (currentIndex < 0)
+        {
+            currentIndex = materials.Length - 1;
+        }
+
+        meshRenderer.material = materials[currentIndex];
+    }
+}
